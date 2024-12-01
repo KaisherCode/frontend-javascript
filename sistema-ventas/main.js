21b21eb1f06499d869c4d0f0731da72a87677f38
Wilson Dev <hch.wilson@hynkor.com>
2024-12-01 17:35:34 -0500

    feat (main.js): add product list

---
+++ sistema-ventas/main.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        title: "Hamburguesa la hynko",
+        price: 12.90,
+        image: "https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Hamburguesa clásica hynko",
+        price: 17.90,
+        image: "https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Hamburguesa hynko",
+        price: 19.90,
+        image: "https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Inka hynko sabor original 500ml",
+        price: 5.90,
+        image: "https://images.unsplash.com/photo-1623944890676-9d5d57a6f7e9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Papas fritas hynko",
+        price: 5.90,
+        image: "https://images.unsplash.com/photo-1529259266118-cf22737f713f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+]
+
+const showMenu = () => products
