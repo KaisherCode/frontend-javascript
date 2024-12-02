d7507fb2071bc2aa17e3959a5b7cd2147345f012
Wilson Dev <hch.wilson@hynkor.com>
2024-12-02 01:46:10 -0500

    feat (main.js): add orderProduct

--- sistema-ventas/main.js
+++ sistema-ventas/main.js
@@ -1,3 +1,11 @@
+const user = {
+  userName: "Kaisher",
+  edad: 32,
+  deuda: 0
+}
+
+const order = []
+
 const showMenu = () => {
   console.log(`CÓDIGO - NOMBRE PRODUCTO - PRECIO`)
 
@@ -6,5 +14,14 @@ const showMenu = () => {
   }
 }
 
+const orderProduct = cod => {
+  if (!cod) return "Ingrese un código válido"
 
+  const productFound = products.find(product => product.id === cod)
+  if (!productFound) return "El producto no existe."
+
+  order.push(productFound)
+  console.log("El producto ha sido agregado a su pedido. Su pedido es:")
+  return order
+}
 
