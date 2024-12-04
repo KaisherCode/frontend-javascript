a16227fabfefb5da0b10deaf4a69c6b516e15c37
Wilson Dev <hch.wilson@hynkor.com>
2024-12-04 15:59:16 -0500

    feat (app.js): añade asignación de estilos con javascript

---
+++ manipulacion-del-DOM/proyectos/toppings-de-pizza/app.js
@@ -0,0 +1,7 @@
+
+const firstTopping = document.querySelector('.topping');
+console.log(firstTopping.style);
+
+firstTopping.style.backgroundColor = "#FBDCE2";
+firstTopping.style.color = "#0ACDFF";
+firstTopping.style.textTransform = "uppercase";
