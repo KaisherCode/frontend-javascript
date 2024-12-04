c9ef83c233ff97e18acf251e52faa07d9946e822
Wilson Dev <hch.wilson@hynkor.com>
2024-12-04 18:31:52 -0500

    fix (app.js): add como acceder al texto del DOM

--- manipulacion-del-DOM/proyectos/toppings-de-pizza/app.js
+++ manipulacion-del-DOM/proyectos/toppings-de-pizza/app.js
@@ -5,3 +5,14 @@ console.log(firstTopping.style);
 // firstTopping.style.backgroundColor = "#FBDCE2";
 // firstTopping.style.color = "#0ACDFF";
 // firstTopping.style.textTransform = "uppercase";
+
+const listToppings = document.getElementById('list-toppings');
+
+console.log('> innerText');
+console.log(listToppings.innerText);
+
+console.log('> textContent');
+console.log(listToppings.textContent);
+
+console.log('> innerHTML');
+console.log(listToppings.innerHTML);
